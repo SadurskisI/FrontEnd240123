ef44742e4ffffc3f1e270562d34fa8128803c1b4
SadurskisI <volk741852@gmail.com>
2023-03-29 00:52:57 +0300

    Main commit

---
+++ HomeworkFE/Homework5/scripts/script.js
@@ -0,0 +1,40 @@
+
+// Написать программу, которая через prompt считывает число и выводит в консоль число равное 10% от введенного числа
+// 
+// const num = prompt("Insert a number")
+// console.log(num * 0.1);
+
+
+// Написать программу, которая получает два числа и выводит наименьшее
+// 
+// const firstNum = prompt("Insert first number")
+// const secondNum = prompt("Insert second number")
+// 
+// if (firstNum > secondNum) {
+//     console.log(secondNum +" is smaller number");
+// } else {
+//     console.log(firstNum +" is smaller number");
+// }
+
+// Написать программу, которая считывает через prompt число и выводит одно из следующих сообщений: ‘Число меньше 100’, ‘Число больше 100’ или ‘Число равно 100’
+// 
+// const checkNum = prompt("Insert a number for check")
+// 
+// if (checkNum > 100) {
+//     console.log("Число больше 100");
+// } else if (checkNum < 100) {
+//     console.log("Число меньше 100");
+// } else { 
+//     console.log("Число равно 100");
+// }
+
+
+// Написать программу, которая запрашивает у пользователя его имя и возраст (в годах) и выводит в консоль сообщение ‘Hello, <name>’, если пользователь совершеннолетний, или ‘Hi, <name>’, если пользователь несовершеннолетний.
+// const firstName = prompt("insert your name")
+// const age = prompt("insert your age in numbers")
+// 
+//   if (age >= 18) {
+//     console.log("Hello, "+firstName);
+// } else {
+//     console.log("Hi, "+firstName);
+// }
